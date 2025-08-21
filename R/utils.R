4dd0acc8a21f94cc7b81e55b7e81131db206df93
Chandler Lutz <chandler.lutz@gmail.com>
2025-08-21 09:13:12 -0400

    Remove depricated chunk2() function.

--- R/utils.R
+++ R/utils.R
@@ -1,5 +1,2 @@
 f_stopifnot <- function(condition, msg) 
   if (!all(condition)) stop(msg)
-
-## https://stackoverflow.com/a/16275428
-chunk2 <- function(x, n) split(x, cut(seq_along(x), n, labels = FALSE))
